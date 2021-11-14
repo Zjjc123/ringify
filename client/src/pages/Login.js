c76c5b496f5c6d413d710f4877cfd29b3e7c9117
Jason Zhang <zjjc123@hotmail.com>
2021-11-13 23:51:56 -0800

    Redirect URL

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AUTH_URL =
-    "https://accounts.spotify.com/authorize?client_id=9149f32554f7425bac1191a602bb83e7&response_type=code&redirect_uri=http://localhost:3000&scope=user-follow-read";
+    "https://accounts.spotify.com/authorize?client_id=9149f32554f7425bac1191a602bb83e7&response_type=code&redirect_uri=https://zjjc123.github.io/ringify&scope=user-follow-read";
 
 export default function Login() {
     return (
